5255eeeb6eca012116fe31387539df56de42a041
buiminhhai1 <ngovietduc20088@gmail.com>
2019-07-11 00:05:26 +0700

    Add customer ejs, Sub file head header and so on

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('customer/index', { title: 'Express' });
+  next();
 });
 
 router.get('/login', function(req, res, next) {
   res.render('customer/login', { title: 'Express' });
+  next();
 });
 
+router.get('/blank', (req,res,next)=>{
+  res.render('customer/blank');
+  next();
+})
 
 module.exports = router;
