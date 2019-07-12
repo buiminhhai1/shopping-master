10cc24587c3c18492a21143325e7b766bd195e9d
buiminhhai1 <ngovietduc20088@gmail.com>
2019-07-12 10:54:35 +0700

    design database

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 
+
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
