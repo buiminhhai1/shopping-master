6c23663c649574c87452ec2888fead7b63e7a816
buiminhhai1 <ngovietduc20088@gmail.com>
2019-07-16 15:22:44 +0700

    add router addmin page

--- routes/admin.js
+++ routes/admin.js
@@ -11,5 +11,39 @@ router.get('/', function(req, res, next) {
   next();
 });
 
+router.get("/productlist", (req,res,next)=>{
+  res.render('admin/product/products');
+  next();
+});
+
+router.get("/product/:product", (req,res,next) =>{
+  res.render("admin/product/productdetail");
+  next();
+}); 
+
+router.get("/brandlist",(req,res,next)=>{
+  res.render("admin/brand/brand");
+  next();
+});
+
+router.get("/orderlist", (req,res,next) => {
+  res.render("/admin/order/orders");
+  next();
+});
+
+// router order detail
+
+router.get("/salesfigures", (req,res,next)=>{
+  res.render("/admin/salesfigures");
+  next();
+});
+
+router.get("/supplierlist", (req,res,next)=>{
+  res.render("admin/supplierlist");
+  next();
+});
+//supplier detail
+
+
 module.exports = router;
 
