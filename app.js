c21752962fd47d2ef834593baa3baaaf7bceb027
Thanh Hai <caothanhhai430@gmail.com>
2019-07-21 19:59:51 +0700

    add feature upload image

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+const multer = require('multer');
 
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
@@ -138,6 +139,69 @@ app.delete("/admin/supplier/:id", (req,res) =>{
       res.json(result);
   });
 });
+
+
+
+// Set The Storage Engine
+const storage = multer.diskStorage({
+  destination: './public/admin/uploads/',
+  filename: function(req, file, cb){
+    cb(null,file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+// Init Upload
+const upload = multer({
+  storage: storage,
+  limits:{fileSize: 1000000},
+  fileFilter: function(req, file, cb){
+    checkFileType(file, cb);
+  }
+}).single('myImage');
+
+// Check File Type
+function checkFileType(file, cb){
+  // Allowed ext
+  const filetypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if(mimetype && extname){
+    return cb(null,true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
+
+
+app.get('/upload', (req, res) => res.render('admin/supplier/testuploadimage'));
+
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if(err){
+      res.render('admin/supplier/testuploadimage', {
+        msg: err
+      });
+    } else {
+      if(req.file == undefined){
+        res.render('admin/supplier/testuploadimage', {
+          msg: 'Error: No File Selected!'
+        });
+      } else {
+        res.render('admin/supplier/testuploadimage', {
+          msg: 'File Uploaded!',
+          file: `admin/uploads/${req.file.filename}`
+        });
+      }
+    }
+  });
+});
+
+
+
+
 ////
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
